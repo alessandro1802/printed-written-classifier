a6187e2f11a51102cae8604c5b1b47ae97447170
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-07-02 13:43:37 +0200

    updated inference file

--- inference.py
+++ inference.py
@@ -6,30 +6,30 @@ class PrintedWrittenClassifier():
     def __init__(self, model_path):
         self.model = tf.keras.models.load_model(model_path)
     
-    def preprocess(image):
+    def preprocess(self, image):
         IMG_SIZE = (256, 256)
         im = cv2.resize(image, IMG_SIZE, interpolation = cv2.INTER_AREA)
         im = np.expand_dims(im, axis = 0)
         return im
     
-    def predict(img):
+    def predict(self, img):
         return self.model.predict(img)[0, 0]
     
-    def postprocess(y_pred):
+    def postprocess(self, y_pred):
         # printed
-        if y_pred > 0.5:
+        if y_pred < 0.5:
             return 0
         # written    
         return 1
 
 if __name__ == "__main__":
-    model_path = "models/printed-written-classifier.h5"
+    model_path = "./model.h5"
     classifier = PrintedWrittenClassifier(model_path)
-    
+ 
     image_path = "test_images/printed.tif"
     image = cv2.imread(image_path)
     image = classifier.preprocess(image)
 
-    y_pred = classifier.predict(model, image)
+    y_pred = classifier.predict(image)
     output = classifier.postprocess(y_pred)
     assert output == 0
